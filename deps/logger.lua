0f549a72e56708ef0c9cf89131faa7177f2825ab
Daniel Barney <daniel@pagodabox.com>
2015-05-15 09:49:53 -0600

    added a blank logger file

---
+++ deps/logger.lua
@@ -0,0 +1,20 @@
+-- -*- mode: lua; tab-width: 2; indent-tabs-mode: 1; st-rulers: [70] -*-
+-- vim: ts=4 sw=4 ft=lua noet
+----------------------------------------------------------------------
+-- @author Daniel Barney <daniel@pagodabox.com>
+-- @copyright 2015, Pagoda Box, Inc.
+-- @doc
+--
+-- @end
+-- Created :   15 May 2015 by Daniel Barney <daniel@pagodabox.com>
+----------------------------------------------------------------------
+
+exports.name = "pagodabox/logger"
+exports.version = "0.1.0"
+exports.description = "Logger that supports file and console based endpoints"
+exports.tags = {"logger"}
+exports.license = "MIT"
+exports.author =
+  	{name = "Daniel Barney"
+  	,email = "daniel@pagodabox.com"}
+exports.homepage = "https://github.com/pagodabox/tag/blob/master/deps/logger.lua"
