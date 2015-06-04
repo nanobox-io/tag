24769daef20d975394e4d2f3780a0bb493cfbde5
Daniel Barney <daniel@pagodabox.com>
2015-06-04 10:35:35 -0600

    fixing indentation

--- lib/system/manager.lua
+++ lib/system/manager.lua
@@ -16,26 +16,26 @@ local log = require('logger')
 local Manager = Cauterize.Supervisor:extend()
 
 function Manager:_manage()
-	local node_name = Cauterize.Fsm.call('config', 'get', 'node_name')
-	local nodes_in_cluster = Cauterize.Fsm.call('config', 'get',
-		'nodes_in_cluster')
-	local alive_systems = nodes_in_cluster[node_name].systems
-	local enabled = 0
+  local node_name = Cauterize.Fsm.call('config', 'get', 'node_name')
+  local nodes_in_cluster = Cauterize.Fsm.call('config', 'get',
+    'nodes_in_cluster')
+  local alive_systems = nodes_in_cluster[node_name].systems
+  local enabled = 0
   if alive_systems then
-  	local systems = Cauterize.Fsm.call('config', 'get', 'systems')
+    local systems = Cauterize.Fsm.call('config', 'get', 'systems')
     for _,system_name in pairs(alive_systems) do
-    	local system_data = systems[system_name]
-    	if system_data then
-    		log.info('enabling system',system_name,system_data)
-    		enabled = enabled + 1
-	      self:manage(System,{args = {system_name,system_data}})
-	    else
-	    	log.warning('unknown system',system_name)
-	    end
+      local system_data = systems[system_name]
+      if system_data then
+        log.info('enabling system',system_name,system_data)
+        enabled = enabled + 1
+        self:manage(System,{args = {system_name,system_data}})
+      else
+        log.warning('unknown system',system_name)
+      end
     end
- 	end
- 	if enabled == 0 then
- 		log.info('entering arbitration mode, no systems enabled')
+   end
+   if enabled == 0 then
+     log.info('entering arbitration mode, no systems enabled')
   end
 end
 
